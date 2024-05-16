f9fd6cef8c65f8148d660a5d079fadf205b421cd
TeddyDharma <tedi.dharma84@gmail.com>
2024-05-16 10:21:40 +0800

    adding py file

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from main import *
+from model import *
 
 # get_feature = feature_extraction_tabular("./data/genres_original/blues/blues.00001.wav")
-print(predict_tabular("./data/genres_original/rock/rock.00001.wav"))
+print(predict_tabular("./data/genres_original/hiphop/hiphop.00001.wav"))
